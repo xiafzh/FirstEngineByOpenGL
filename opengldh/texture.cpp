40a7277ff6f77f045180f74c9b3f0e595979694b
xiafzh <zhangxiafei5217@163.com>
2019-03-19 20:44:42 +0800

    add libpng

--- opengldh/texture.cpp
+++ opengldh/texture.cpp
@@ -1,6 +1,15 @@
 #include "texture.h"
+#include "png.h"
+
+#pragma comment(lib, "libpng16.lib")
+#pragma comment(lib, "zlib.lib")
 
 CTexture* CTexture::LoadTexture(const std::string& path)
 {
+	png_structp png_ptr = NULL;
+	png_infop info_ptr = NULL;
 
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+	return nullptr;
 }
